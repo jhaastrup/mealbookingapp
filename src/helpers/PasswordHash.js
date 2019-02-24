3739e918a09a055b8b6913247259dc08c0a56a0e
jhaastrup <jhaastrup21@gmail.com>
2019-02-24 14:45:11 +0100

    Add login route

--- src/helpers/PasswordHash.js
+++ src/helpers/PasswordHash.js
@@ -4,6 +4,10 @@ class PasswordHash {
   static hash(password, salt) {
     return bcrypt.hashSync(password, salt);
   }
+
+  static compare(password, hashPassword) {
+    return bcrypt.compareSync(password, hashPassword);
+  }
 }
 
 export default PasswordHash;
--- src/middlewares/AuthMiddleware.js
+++ src/middlewares/AuthMiddleware.js
@@ -68,6 +68,38 @@ class AuthMiddleware {
 
     next();
   }
+
+  static loginValidation(req, res, next) {
+    const { email, password } = req.body;
+    let errors = false;
+    if (!emailValidation(email)) {
+      errors = {
+        statusText: 'Bad Request',
+        status: 400,
+        message: 'Invalid email format',
+      };
+    }
+    if (!email) {
+      errors = {
+        statusText: 'Bad Request',
+        status: 400,
+        message: 'Email cannot be empty',
+      };
+    }
+    if (!password) {
+      errors = {
+        statusText: 'Bad Request',
+        status: 400,
+        message: 'Password cannot be empty',
+      };
+    }
+
+    if (errors) {
+      return res.status(400).send(errors);
+    }
+
+    next();
+  }
 }
 
 export default AuthMiddleware;
